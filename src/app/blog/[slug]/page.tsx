df46a4201ac020cc707809d882361c63ef63c4ed
ys <ys@betweak.com>
2025-06-17 10:41:11 +0900

    image

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,11 @@
+// /app/blog/[slug]/page.tsx
+// ==> /blog/:parameter
+// eg. (/blog/1, /blog/hello-world)
+
+export default function BlogDetailPage() {
+	return (
+		<div>
+			<h1 className="text-4xl font-bold">BlogDetailPage</h1>
+		</div>
+	);
+}
