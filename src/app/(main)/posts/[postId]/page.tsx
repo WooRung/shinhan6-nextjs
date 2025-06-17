5fdca9a4703f2ed9effff97f31e0341fbf93c930
ys <ys@betweak.com>
2025-06-17 14:43:10 +0900

    add notebook

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -1,6 +1,20 @@
 // /app/(main)/posts/[postId]/page.tsx
 
-export default async function PostDetailPage() {
+/**
+ * 서버에서 URL Parameter 받아오기
+ * ==> props로 받아온다.
+ */
+export interface PostDetailPageProps {
+  params: Promise<{
+    postId: string;
+  }>;
+}
+
+export default async function PostDetailPage({ params }: PostDetailPageProps) {
+  const data = await params;
+
+  const { postId } = data;
+
   return (
     <div className="container mx-auto text-center p-8">
       <h1 className="text-3xl font-bold underline">게시글 상세 페이지.</h1>
